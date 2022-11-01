2b80fa78dc2757b7f900c2fd2651b6694ed7271f
fidamohammed <fidammp@gmail.com>
2022-11-01 14:11:14 +0000

    added test case

--- app/src/test/java/com/example/quotesapplication/data/repository/RepositoryImplTest.kt
+++ app/src/test/java/com/example/quotesapplication/data/repository/RepositoryImplTest.kt
@@ -3,12 +3,14 @@ package com.example.quotesapplication.data.repository
 import com.example.quotesapplication.data.api.NetworkDetails
 import com.example.quotesapplication.data.model.QuotesItemModel
 import kotlinx.coroutines.runBlocking
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.whenever
+import retrofit2.Response
 
 class RepositoryImplTest{
     private lateinit var repositoryImpl: RepositoryImpl
@@ -27,5 +29,13 @@ class RepositoryImplTest{
             .thenReturn(arrayListOf(QuotesItemModel(id = "111")))
         val result = repositoryImpl.getQuotes()
         assertEquals(arrayListOf(QuotesItemModel(id = "111")), result)
+    }
+
+    @Test
+    fun `getQuotes with empty response`() = runBlocking{
+        whenever(networkDetails.getQuotes())
+            .thenReturn(arrayListOf())
+        val result = repositoryImpl.getQuotes()
+        assertTrue(result.isEmpty())
     }
 }
